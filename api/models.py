1fa2d2677d26d4f7e412a0907855f9351da3d361
yousefshref <yb200a5@gmail.com>
2024-06-13 10:20:39 +0300

    dsa

--- api/models.py
+++ api/models.py
@@ -39,6 +39,10 @@ class ManagerProfile(models.Model):
   can_academy = models.BooleanField(default=False)
   can_courts = models.BooleanField(default=False)
 
+  trainer_limit = models.IntegerField(default=0, null=True, blank=True)
+  academy_limit = models.IntegerField(default=0, null=True, blank=True)
+  courts_limit = models.IntegerField(default=0, null=True, blank=True)
+
   def __str__(self):
     return self.user.username
   
